7980543e5bcedef37de78fb03f0c1a172472f824
renansdf <rnn.frts@gmail.com>
2020-11-23 18:53:42 -0300

    fixed function loading on every page. added function to handle more-filters reveal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,26 +2,31 @@
   $(document).ready(function () {
 
     var priceSlider = document.getElementById('price-range');
-
-    noUiSlider.create(priceSlider, {
-      start: [20, 80],
-      connect: true,
-      range: {
-        'min': 0,
-        'max': 100
-      }
-    });
-
+    if (priceSlider) {
+      noUiSlider.create(priceSlider, {
+        start: [20, 80],
+        connect: true,
+        range: {
+          'min': 0,
+          'max': 100
+        }
+      });
+    }
 
     var sizeSlider = document.getElementById('size-range');
+    if (sizeSlider) {
+      noUiSlider.create(sizeSlider, {
+        start: [20, 80],
+        connect: true,
+        range: {
+          'min': 0,
+          'max': 100
+        }
+      });
+    }
 
-    noUiSlider.create(sizeSlider, {
-      start: [20, 80],
-      connect: true,
-      range: {
-        'min': 0,
-        'max': 100
-      }
+    $('#more-filters').click(() => {
+      $('#more-filters-container').toggleClass('active');
     });
 
   });
