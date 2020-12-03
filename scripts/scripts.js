60b0746528b66eb562cad0a22c2ef73d70f4bc01
renansdf <rnn.frts@gmail.com>
2020-12-03 20:51:17 -0300

    updated range slider functions and created handles to update DOM with range slider value upon interaction

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,21 @@
     var priceSlider = document.getElementById('price-range');
     if (priceSlider) {
       noUiSlider.create(priceSlider, {
-        start: [20, 80],
+        start: [1, 25],
+        tooltips: [wNumb({
+          thousand: '.',
+          prefix: '€',
+          suffix: ' mil'
+        }), wNumb({
+          thousand: '.',
+          prefix: '€',
+          suffix: ' mil'
+        })],
         connect: true,
+        step: 1,
         range: {
-          'min': 0,
-          'max': 100
+          'min': 1,
+          'max': 25
         }
       });
     }
@@ -16,17 +26,45 @@
     var sizeSlider = document.getElementById('size-range');
     if (sizeSlider) {
       noUiSlider.create(sizeSlider, {
-        start: [20, 80],
+        start: [100, 600],
         connect: true,
+        tooltips: [wNumb({
+          suffix: ' sqft'
+        }), wNumb({
+          suffix: ' sqft'
+        })],
+        step: 100,
         range: {
-          'min': 0,
-          'max': 100
+          'min': 100,
+          'max': 600
         }
       });
     }
 
     $('#more-filters').click(() => {
       $('#more-filters-container').toggleClass('active');
+    });
+
+    priceSlider.noUiSlider.on('end', function (values) {
+      $('#price-min').val(values[0]);
+      $('#price-max').val(values[1]);
+    });
+
+    sizeSlider.noUiSlider.on('end', function (values) {
+      $('#size-min').val(values[0]);
+      $('#size-max').val(values[1]);
+    });
+
+
+
+    $('#price-range').click(function () {
+      $('#price-min').val(priceSlider.noUiSlider.get()[0]);
+      $('#price-max').val(priceSlider.noUiSlider.get()[1]);
+    });
+
+    $('#size-range').click(function () {
+      $('#size-min').val(sizeSlider.noUiSlider.get()[0]);
+      $('#size-max').val(sizeSlider.noUiSlider.get()[1]);
     });
 
   });
